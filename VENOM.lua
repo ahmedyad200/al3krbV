6da8209c7c4a25d3a94d94c78dbaf35154dc01ad
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-12-16 22:29:29 +0200

    VENOM.lua

--- VENOM.lua
+++ VENOM.lua
@@ -152,7 +152,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/devbnds/lole/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/ahmedyad200/al3krbV/main/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -1870,7 +1870,7 @@ echo '╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╸\n 🔌l •⊱ 『 مـد
 end
 if text == 'تحديث السورس ' and Devban(msg) then 
 os.execute('rm -rf VENOM.lua')
-os.execute('wget https://raw.githubusercontent.com/devbnds/lole/main/VENOM.lua')
+os.execute('wget https://raw.githubusercontent.com/ahmedyad200/al3krbV/main/VENOM.lua')
 send(msg.chat_id_, msg.id_,' ● تم تحديث السورس')
 dofile('VENOM.lua')  
 end
@@ -3639,7 +3639,7 @@ end
 return false
 end
 os.execute('rm -rf VENOM.lua')
-os.execute('wget https://raw.githubusercontent.com/devbnds/lole/main/VENOM.lua')
+os.execute('wget https://raw.githubusercontent.com/ahmedyad200/al3krbV/main/VENOM.lua')
 send(msg.chat_id_, msg.id_,' ● تم تحديث السورس')
 dofile('VENOM.lua')  
 end
@@ -7195,7 +7195,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if Devban(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/devbnds/lole/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/ahmedyad200/al3krbV/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -7233,7 +7233,7 @@ t = " ● الملف  ⋙ "..file.."\n ● تم تعطيل ملف \n"
 else
 t = " ● بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/devbnds/lole/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/ahmedyad200/al3krbV/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -7253,7 +7253,7 @@ t = " ● بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ● الملف  ⋙ "..file.."\n ● تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/devbnds/lole/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/ahmedyad200/al3krbV/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
